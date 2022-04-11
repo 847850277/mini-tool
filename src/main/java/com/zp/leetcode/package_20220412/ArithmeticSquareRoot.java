b654facaf6a74a1f5cec1be52f27553a55e13d11
zhengpeng <847850277@qq.com>
2022-04-11 09:26:41 +0800

    算术根

---
+++ src/main/java/com/zp/leetcode/package_20220412/ArithmeticSquareRoot.java
@@ -0,0 +1,19 @@
+package com.zp.leetcode.package_20220412;
+
+/**
+ * @author zhengpeng
+ * @date 2022-04-11 09:22
+ * 算术平方根
+ **/
+public class ArithmeticSquareRoot {
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
