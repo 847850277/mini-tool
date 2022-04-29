07e8d4f68d4e2a3c7a77b0417e5e7228e0605454
zhengpeng <847850277@qq.com>
2022-04-29 16:34:54 +0800

    feature:颠倒二进制位

--- src/main/java/com/zp/leetcode/package_2022/april/april0429/ReverseBits.java
+++ src/main/java/com/zp/leetcode/package_2022/april/april0429/ReverseBits.java
@@ -8,14 +8,16 @@ package com.zp.leetcode.package_2022.april.april0429;
 public class ReverseBits {
 
 
-    public String convertToTitle(int columnNumber) {
-        StringBuffer sb = new StringBuffer();
-        while (columnNumber > 0) {
-            int a0 = (columnNumber - 1) % 26 + 1;
-            sb.append((char)(a0 - 1 + 'A'));
-            columnNumber = (columnNumber - a0) / 26;
+    public long binaryToLong(String s){
+
+        int size = s.length();
+        long sum = 0L;
+        for (int i = 0; i < size;i++) {
+           if(s.charAt(size - i - 1) == '1'){
+               sum += Math.pow(2,i);
+           }
         }
-        return sb.reverse().toString();
+        return sum;
     }
 
     /**
@@ -23,22 +25,26 @@ public class ReverseBits {
      * @param n
      * @return
      */
-    public String to32Binary(int n){
+    @SuppressWarnings("Duplicates")
+    public String to32Binary(long n){
 
-        StringBuilder sb = new StringBuilder("00000000000000000000000000000000");
+        StringBuilder sb = new StringBuilder();
+        //次方数
         int index = 0;
-        //System.out.println(n / 2);
-        for (int i = 0; i< 32 ; i++){
-            //偶数
-            if(n % 2 == 0){
-
-
-            //奇数
+        while (n > 0){
+            long a0 = n % 2;
+            if(a0 / (Math.pow(2,index)) == 0){
+                sb.append("0");
             }else {
-
+                sb.append("1");
             }
+            n = (n - a0) / 2;
+            index ++;
         }
-        return "";
+        for (int i = index; i < 32; i++) {
+            sb.append("0");
+        }
+        return sb.reverse().toString();
     }
 
     /**
@@ -46,25 +52,62 @@ public class ReverseBits {
      * @param n
      * @return
      */
-    public int reverseBits(int n) {
-        //转换为32位
-        //反转
-        //然后再把32位转换位整数返回
+    @SuppressWarnings("Duplicates")
+    public long reverseBits(long n) {
+        StringBuilder sb = new StringBuilder();
+        //次方数
+        int index = 0;
+        while (n > 0){
+            long a0 = (n % 2);
+            if(a0 / (Math.pow(2,index)) == 0){
+                sb.append("0");
+            }else {
+                sb.append("1");
+            }
+            n = (n - a0) / 2;
+            index ++;
+        }
+        for (int i = index; i < 32; i++) {
+            sb.append("0");
+        }
+
+        // 求和
+        StringBuilder newB = sb.reverse();
+        long sum = 0L;
+        for (int j = 0; j < 32; j++) {
+            final char c = newB.charAt(j);
+            if(c == '1'){
+                sum += Math.pow(2,j);
+            }
+        }
+        return sum;
+    }
+
+    public int reverseBits2(int n) {
         int ans = 0;
         for (int i = 0; i < 32; i++) {
-
-
-
+            int t = (n >> i) & 1;
+            if (t == 1) {
+                ans |= (1 << (31 - i));
+            }
         }
         return ans;
     }
 
     public static void main(String[] args) {
         ReverseBits reverseBits = new ReverseBits();
-
-        //System.out.println(reverseBits.reverseBits(00000010100101000001111010011100));
         //System.out.println(reverseBits.reverseBits(43261596));
-        System.out.println(reverseBits.to32Binary(43261596));
+        //System.out.println(reverseBits.reverseBits(4294967293L));
+        //System.out.println(reverseBits.reverseBits(3221225471));
+        //System.out.println(reverseBits.reverseBits2(3221225471));
+        System.out.println(reverseBits.reverseBits(4294967293L));
+        //System.out.println(reverseBits.binaryToLong("11111111111111111111111111111101"));
+        System.out.println(reverseBits.to32Binary(4294967293L));
+        //System.out.println(reverseBits.binaryToLong("00111111111111111111111111111111"));
+        //System.out.println(reverseBits.to32Binary(20));
+        //System.out.println(reverseBits.to32Binary(3));
+        //System.out.println(reverseBits.to32Binary(100));
+        //System.out.println(reverseBits.to32Binary(4));
 
     }
 }
