b0ae76d973d80f100679e976f75d0e9dc1aad7c1
zhengpeng <847850277@qq.com>
2022-05-08 16:51:58 +0800

    feature:存在重复元素，todo，用队列实现栈

---
+++ src/main/java/com/zp/leetcode/package_2022/may/may08/DuplicateElement.java
@@ -0,0 +1,35 @@
+package com.zp.leetcode.package_2022.may.may08;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhengpeng
+ * @date 2022-05-08 16:47
+ * 重复元素
+ **/
+public class DuplicateElement {
+    /**
+     * 利用map
+     * @param nums
+     * @return
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> keyCounter = new HashMap((int) (nums.length * 1.75));
+        for (int num : nums) {
+            if(keyCounter.containsKey(num)){
+                return true;
+            }
+            keyCounter.put(num,num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{1,2,3,4,5};
+        DuplicateElement duplicateElement = new DuplicateElement();
+        System.out.println(duplicateElement.containsDuplicate(array));
+
+    }
+}
