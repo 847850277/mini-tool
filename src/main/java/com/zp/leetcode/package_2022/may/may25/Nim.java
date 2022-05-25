3410a293943090ce485c50928a399e32531d564a
zhengpeng <847850277@qq.com>
2022-05-25 16:19:44 +0800

    feature:区域检索和数组不可变

---
+++ src/main/java/com/zp/leetcode/package_2022/may/may25/Nim.java
@@ -0,0 +1,16 @@
+package com.zp.leetcode.package_2022.may.may25;
+
+/**
+ * @author zhengpeng
+ * @date 2022/5/25 4:04 下午
+ * nim游戏
+ **/
+public class Nim {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
