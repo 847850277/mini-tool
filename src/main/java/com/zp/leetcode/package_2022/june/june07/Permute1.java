ec38397e941598dd3dadc552c5b4c5bcd2f59081
zhengpeng <847850277@qq.com>
2022-06-08 16:45:19 +0800

    feature:有效的回旋镖

--- src/main/java/com/zp/leetcode/package_2022/june/june07/Permute1.java
+++ src/main/java/com/zp/leetcode/package_2022/june/june07/Permute1.java
@@ -1,8 +1,8 @@
 package com.zp.leetcode.package_2022.june.june07;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author zhengpeng
@@ -10,15 +10,16 @@ import java.util.List;
  **/
 public class Permute1 {
     List<List<Integer>> result = new ArrayList<>();
-    LinkedList<Integer> path = new LinkedList<>();
+    //LinkedList<Integer> path = new LinkedList<>();
+    Stack<Integer> stack = new Stack<>();
 
     public List<List<Integer>> permute(int[] nums) {
         if (nums.length == 0) return result;
-        backtrack(nums, path);
+        backtrack(nums, stack);
         return result;
     }
 
-    public void backtrack(int[] nums, LinkedList<Integer> path) {
+    public void backtrack(int[] nums, Stack<Integer> path) {
         if (path.size() == nums.length) {
             result.add(new ArrayList<>(path));
         }
@@ -27,9 +28,17 @@ public class Permute1 {
             if (path.contains(nums[i])) {
                 continue;
             }
-            path.add(nums[i]);
+            path.push(nums[i]);
             backtrack(nums, path);
-            path.removeLast();
+            path.pop();
         }
+    }
+
+    public static void main(String[] args) {
+        //int[] array = new int[]{1,2,3,4,5};
+        int[] array = new int[]{1,2,3};
+        Permute1 permute = new Permute1();
+        System.out.println(permute.permute(array));
+
     }
 }
