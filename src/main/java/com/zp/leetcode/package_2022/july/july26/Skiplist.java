c37bed7fdb4a801a485347725622ba3abce987c9
zhengpeng <847850277@qq.com>
2022-07-30 17:20:55 +0800

    feature:设计跳表

--- src/main/java/com/zp/leetcode/package_2022/july/july26/Skiplist.java
+++ src/main/java/com/zp/leetcode/package_2022/july/july26/Skiplist.java
@@ -1,113 +1,88 @@
 package com.zp.leetcode.package_2022.july.july26;
 
-import java.util.Arrays;
 import java.util.Random;
 
 /**
  * @author zhengpeng
  * @date 2022/7/26 1:55 下午
  * 设计跳表
+ * https://www.bilibili.com/video/BV1gN4y1T7hd
  **/
 public class Skiplist {
-
-    public static class SkiplistNode{
-
+    /**
+     * 层级，最高10层
+     */
+    int level = 10;
+    class Node {
         int val;
-        SkiplistNode[] forward;
-
-        public SkiplistNode(int val, int maxLevel) {
+        Node[] datas = new Node[level];
+        public Node(int val) {
             this.val = val;
-            this.forward = new SkiplistNode[maxLevel];
         }
     }
-
+    Random random = new Random();
+    Node head = new Node(-1);
 
     public static void main(String[] args) {
+
         Skiplist skiplist = new Skiplist();
-        skiplist.add(1);
-        skiplist.add(2);
+        skiplist.add(26);
+        skiplist.add(25);
         skiplist.add(3);
-        skiplist.add(4);
-        skiplist.add(5);
         skiplist.add(6);
-        skiplist.add(7);
-        skiplist.add(8);
         skiplist.add(9);
-        skiplist.add(10);
-        skiplist.add(1);
-        skiplist.add(2);
-        skiplist.add(3);
-        skiplist.add(4);
-        skiplist.add(5);
-        skiplist.add(6);
+        skiplist.add(21);
+        skiplist.add(19);
         skiplist.add(7);
-        skiplist.add(8);
-        skiplist.add(9);
-        skiplist.add(10);
+        skiplist.add(12);
 
-
+        skiplist.search(9);
+        skiplist.add(17);
+        skiplist.search(17);
 
     }
 
-    static final int MAX_LEVEL = 32;
-    static final double P_FACTOR = 0.25d;
-
-    private SkiplistNode head;
-    private int level;
-    private Random random;
-
-    /**
-     * 构造函数
-     */
-    public Skiplist() {
-        this.head = new SkiplistNode(-1,MAX_LEVEL);
-        this.level = 0;
-        this.random = new Random();
-    }
-
-    /**
-     * 添加元素
-     * @param num
-     */
-    public void add(int num) {
-        SkiplistNode[] update  = new SkiplistNode[MAX_LEVEL];
-        Arrays.fill(update,head);
-        SkiplistNode curr = this.head;
-        for(int i = level; i >= 0; i--){
-            //找到第i层小于且最接近num的元素
-            while(curr.forward[i] != null && curr.forward[i].val < num){
-                curr = curr.forward[i];
-            }
-            update[i] = curr;
-        }
-        int lv = randomLevel();
-        level = Math.max(level,lv);
-        SkiplistNode newNode = new SkiplistNode(num,lv);
-        for (int i = 0;i < lv;i++){
-            newNode.forward[i] = update[i].forward[i];
-            update[i].forward[i] = newNode;
-        }
-    }
-
-    /**
-     * 随机层数
-     * @return
-     */
-    private int randomLevel() {
-        int lv = 1;
-        while(random.nextDouble() < P_FACTOR && lv < MAX_LEVEL){
-            lv++;
-        }
-        return lv;
-    }
 
     public boolean search(int target) {
-        return false;
+        Node[] ns = new Node[level];
+        find(target,ns);
+        return ns[0].datas[0] != null && ns[0].datas[0].val == target;
     }
 
-
-
-    public boolean erase(int num) {
-        return false;
+    public void add(int num) {
+        Node[] ns = new Node[level];
+        find(num,ns);
+        Node node = new Node(num);
+        for(int i = 0; i < level;i++){
+            node.datas[i] = ns[i].datas[i];
+            ns[i].datas[i] = node;
+            if(random.nextInt() == 2){
+                break;
+            }
+        }
     }
+
+    private void find(int num, Node[] ns) {
+        Node cur = head;
+        for(int i = level - 1; i >= 0; i--){
+            while (cur.datas[i] != null && cur.datas[i].val < num){
+                cur = cur.datas[i];
+            }
+            ns[i] = cur;
+        }
+    }
+
+    public boolean erase(int t) {
+        Node[] ns = new Node[level];
+        find(t, ns);
+        Node node = ns[0].datas[0];
+        if (node == null || node.val != t) {
+            return false;
+        }
+        for (int i = 0; i < level && ns[i].datas[i] == node; i++){
+            ns[i].datas[i] = ns[i].datas[i].datas[i];
+        }
+        return true;
+    }
+
 }
