5bc8350cf3c1c0b588cf7d0deef688f3c0eca3c5
zhengpeng <847850277@qq.com>
2022-08-28 22:39:04 +0800

    feature:增加数组转二叉树的工具类

--- src/main/java/com/zp/leetcode/package_2022/august/august27/WidthOfBinaryTree.java
+++ src/main/java/com/zp/leetcode/package_2022/august/august27/WidthOfBinaryTree.java
@@ -11,6 +11,35 @@ import java.util.Queue;
 public class WidthOfBinaryTree {
 
 
+    public static void main(String[] args) {
+        WidthOfBinaryTree tree = new WidthOfBinaryTree();
+        TreeNode root = new TreeNode(1);
+        TreeNode node_3 = new TreeNode(3);
+        TreeNode node_2 = new TreeNode(2);
+        TreeNode node_5 = new TreeNode(5);
+        TreeNode node_9 = new TreeNode(9);
+        TreeNode node_6 = new TreeNode(6);
+        TreeNode node_7 = new TreeNode(7);
+
+        root.left = node_3;
+        root.right = node_2;
+        node_3.left = node_5;
+        node_5.left = node_6;
+        node_2.right = node_9;
+        node_9.left = node_7;
+
+        //System.out.println(tree.widthOfBinaryTree(root));
+        //System.out.println(tree.widthOfBinaryTree1(root));
+        //Integer[] array = new Integer[]{1,2,3,4,5};
+        Integer[] array = new Integer[]{1,3,2,5,null,null,9,6,null,7};
+        final TreeNode convert = arrayToTreeNode(array);
+        System.out.println(tree.widthOfBinaryTree(convert));
+        System.out.println(tree.widthOfBinaryTree1(convert));
+
+
+    }
+
+
     /**
      * 思路
      * 将二叉树的下标存储到数组中、根节点的下标为1，左子树的节点为 2 * i、右子树的下标为 2 * i + 1
@@ -91,5 +120,34 @@ public class WidthOfBinaryTree {
              this.right = right;
          }
      }
+
+
+    public static TreeNode arrayToTreeNode(Integer[] array){
+        if(array.length == 0){
+            return null;
+        }
+        TreeNode root = new TreeNode(array[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean isLeft = true;
+        for(int i = 1; i < array.length; i++){
+            TreeNode node = queue.peek();
+            if(isLeft){
+                if(array[i] != null){
+                    node.left = new TreeNode(array[i]);
+                    queue.offer(node.left);
+                }
+                isLeft = false;
+            }else {
+                if(array[i] != null){
+                    node.right = new TreeNode(array[i]);
+                    queue.offer(node.right);
+                }
+                queue.poll();
+                isLeft = true;
+            }
+        }
+        return root;
+    }
 
 }
