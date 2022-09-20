77fbda9a1a4e3efba37a65809b596d2912c5da31
zhengpeng <847850277@qq.com>
2022-09-20 09:50:43 +0800

    feature: 按照频率将数组升序排序

--- src/main/java/com/zp/leetcode/package2022/september/september19/FrequencySort.java
+++ src/main/java/com/zp/leetcode/package2022/september/september19/FrequencySort.java
@@ -1,7 +1,6 @@
 package com.zp.leetcode.package2022.september.september19;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,14 +13,45 @@ import java.util.Map;
 public class FrequencySort {
 
     public static void main(String[] args) {
-        int[] array = new int[]{};
+        int[] array = new int[]{1,2,2,3,3};
         FrequencySort frequencySort = new FrequencySort();
-        final int[] ints = frequencySort.frequencySort(array);
+        //final int[] ints = frequencySort.frequencySort(array);
+        final int[] ints = frequencySort.frequencySort1(array);
         for (int anInt : ints) {
             System.out.println(anInt);
         }
-
     }
+
+
+    /**
+     * 按照频率将数组升序排序
+     * @param array
+     * @return
+     */
+    public int[] frequencySort1(int[] array) {
+
+        Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+        for (int i : array) {
+            countMap.put(i, countMap.getOrDefault(i, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : array) {
+            list.add(i);
+        }
+        //排序
+        list.sort((a,b) ->{
+            int cnt1 = countMap.get(a), cnt2 = countMap.get(b);
+            return cnt1 != cnt2 ? cnt1 - cnt2 : b - a;
+        });
+
+        int[] result = new int[array.length];
+        for(int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+
 
     public int[] frequencySort(int[] nums) {
         Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
@@ -32,7 +62,7 @@ public class FrequencySort {
         for (int num : nums) {
             list.add(num);
         }
-        Collections.sort(list, (a, b) -> {
+        list.sort((a, b) -> {
             int cnt1 = cnt.get(a), cnt2 = cnt.get(b);
             return cnt1 != cnt2 ? cnt1 - cnt2 : b - a;
         });
---
+++ src/main/java/com/zp/leetcode/package2022/september/september20/CanPartitionKSubsets.java
@@ -0,0 +1,16 @@
+package com.zp.leetcode.package2022.september.september20;
+
+/**
+ * @author zhengpeng
+ * @date 2022/9/20 9:14 上午
+ **/
+public class CanPartitionKSubsets {
+
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+
+
+
+        return false;
+    }
+}
