48d81ae95f272bcbfb4177c621fc2450de55e5b7
zhengpeng <847850277@qq.com>
2022-10-08 09:55:30 +0800

    feature: 优势洗牌

--- src/main/java/com/zp/leetcode/package2022/october/october01/AdvantageCount.java
+++ src/main/java/com/zp/leetcode/package2022/october/october01/AdvantageCount.java
@@ -11,6 +11,21 @@ import java.util.stream.IntStream;
  **/
 public class AdvantageCount {
 
+
+    public static void main(String[] args) {
+        int[] array1 = new int[]{2,7,11,15};
+        int[] array2 = new int[]{1,10,4,11};
+
+        AdvantageCount a = new AdvantageCount();
+        int[] ints = a.advantageCount(array1, array2);
+        for (int anInt : ints) {
+            System.out.print(anInt);
+            System.out.print(",");
+        }
+
+    }
+
+
     /**
      * 田忌赛马
      * @param nums1
@@ -22,9 +37,11 @@ public class AdvantageCount {
         int[] ans = new int[n];
         Arrays.sort(nums1);
         Integer[] ids = IntStream.range(0, n).boxed().toArray(Integer[]::new);
+        //num2索引排序
         Arrays.sort(ids, (i, j) -> nums2[i] - nums2[j]);
         int left = 0, right = n - 1;
         for (int x : nums1){
+            //选择num1元素赋值到索引位置
             ans[x > nums2[ids[left]] ? ids[left++] : ids[right--]] = x;
         }
         return ans;
