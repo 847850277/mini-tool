651ab57c7e364e050cbb1450e90411c40541f8a8
zhengpeng <847850277@qq.com>
2022-10-14 09:51:20 +0800

    feature: 不同的子序列

---
+++ src/main/java/com/zp/leetcode/package2022/october/october14/DistinctSubseqII.java
@@ -0,0 +1,43 @@
+package com.zp.leetcode.package2022.october.october14;
+
+import java.util.Arrays;
+
+/**
+ * @author zhengpeng
+ * @date 2022/10/14 9:48 上午
+ * 不同的子序列
+ **/
+public class DistinctSubseqII {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int distinctSubseqII(String s) {
+        final int MOD = 1000000007;
+        int[] last = new int[26];
+        Arrays.fill(last, -1);
+
+        int n = s.length();
+        int[] f = new int[n];
+        Arrays.fill(f, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                if (last[j] != -1) {
+                    f[i] = (f[i] + f[last[j]]) % MOD;
+                }
+            }
+            last[s.charAt(i) - 'a'] = i;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (last[i] != -1) {
+                ans = (ans + f[last[i]]) % MOD;
+            }
+        }
+        return ans;
+    }
+
+}
