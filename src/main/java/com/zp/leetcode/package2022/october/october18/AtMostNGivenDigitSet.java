e2bd8e92a2003ad903bb5ae82b3bbc45e3b1acf7
zhengpeng <847850277@qq.com>
2022-10-18 09:19:53 +0800

    feature: 最大为n的数字组合

---
+++ src/main/java/com/zp/leetcode/package2022/october/october18/AtMostNGivenDigitSet.java
@@ -0,0 +1,19 @@
+package com.zp.leetcode.package2022.october.october18;
+
+/**
+ * @author zhengpeng
+ * @date 2022/10/18 8:57 上午
+ * 最大为n的数字组合
+ **/
+public class AtMostNGivenDigitSet {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/zp/leetcode/package2022/october/october18/TotalFruit.java
@@ -0,0 +1,45 @@
+package com.zp.leetcode.package2022.october.october18;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhengpeng
+ * @date 2022/10/18 8:58 上午
+ * 水果成篮子
+ **/
+public class TotalFruit {
+
+    public static void main(String[] args) {
+        //int[] array = new int[]{1,2,2,1};
+        int[] array = new int[]{1,2,3,1,1,3};
+        TotalFruit totalFruit = new TotalFruit();
+        System.out.println(totalFruit.totalFruit(array));
+    }
+
+
+    public int totalFruit(int[] fruits) {
+        int len = fruits.length;
+        Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+        int ans = 0, left = 0;
+        for (int right = 0; right < len; right++) {
+            int val = fruits[right];
+            countMap.put(val, countMap.getOrDefault(val, 0) + 1);
+            // 窗口滑动
+            while (countMap.size() > 2) {
+                int leftVal = fruits[left];
+                countMap.put(leftVal, countMap.get(leftVal) - 1);
+                if (countMap.get(leftVal) == 0) {
+                    countMap.remove(leftVal);
+                }
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+
+
+}
