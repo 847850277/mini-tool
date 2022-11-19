09b20bcc4c341b18c084af7fff9878412622a149
zhengpeng <847850277@qq.com>
2022-11-19 11:31:24 +0800

    feature: 找到最高海拔

--- src/main/java/com/zp/leetcode/package2022/november/november19/LargestAltitude.java
+++ src/main/java/com/zp/leetcode/package2022/november/november19/LargestAltitude.java
@@ -25,4 +25,15 @@ public class LargestAltitude {
         }
         return max;
     }
+
+
+    public int largestAltitude1(int[] gain) {
+        int ans = 0, sum = 0;
+        for (int x : gain) {
+            sum += x;
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+
 }
