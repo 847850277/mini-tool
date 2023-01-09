e9257b88330fa3919966db67721dddb2c6801e48
zhengpeng <847850277@qq.com>
2023-01-09 17:12:41 +0800

    feature: 还原排列最少的操作步骤

---
+++ src/main/java/com/zp/leetcode/package2022/december/december30/ExamRoom.java
@@ -0,0 +1,21 @@
+package com.zp.leetcode.package2022.december.december30;
+
+/**
+ * @author zhengpeng
+ * @date 2022/12/30 11:22 上午
+ **/
+public class ExamRoom {
+
+
+    public ExamRoom(int n) {
+
+    }
+
+    public int seat() {
+        return 0;
+    }
+
+    public void leave(int p) {
+
+    }
+}
---
+++ src/main/java/com/zp/leetcode/package2023/january/january01/ReinitializePermutation.java
@@ -0,0 +1,44 @@
+package com.zp.leetcode.package2023.january.january01;
+
+import java.util.Arrays;
+
+/**
+ * @author zhengpeng
+ * @date 2023/1/9 9:15 上午
+ **/
+public class ReinitializePermutation {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int reinitializePermutation(int n) {
+        int[] perm = new int[n];
+        int[] target = new int[n];
+        for (int i = 0; i < n; i++) {
+            perm[i] = i;
+            target[i] = i;
+        }
+        int step = 0;
+        while (true) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                if ((i & 1) != 0) {
+                    arr[i] = perm[n / 2 + (i - 1) / 2];
+                } else {
+                    arr[i] = perm[i / 2];
+                }
+            }
+            perm = arr;
+            step++;
+            if (Arrays.equals(perm, target)) {
+                break;
+            }
+        }
+        return step;
+    }
+
+
+}
