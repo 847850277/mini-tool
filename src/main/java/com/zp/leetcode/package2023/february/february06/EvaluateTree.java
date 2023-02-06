df320873cf63bbadc24f1c18109e73d5de288a36
zhengpeng <847850277@qq.com>
2023-02-06 09:18:12 +0800

    feature: 计算布尔二叉树的值

---
+++ src/main/java/com/zp/leetcode/package2023/february/february06/EvaluateTree.java
@@ -0,0 +1,41 @@
+package com.zp.leetcode.package2023.february.february06;
+
+/**
+ * @author zhengpeng
+ * @date 2023/2/6 9:15 上午
+ * 计算布尔二叉树的值
+ **/
+public class EvaluateTree {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+
+}
