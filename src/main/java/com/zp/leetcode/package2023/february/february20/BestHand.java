5271c034330437c70a14516a321d01cd9ffffccd
zhengpeng <847850277@qq.com>
2023-02-20 09:37:30 +0800

    feature: 最好的扑克手

---
+++ src/main/java/com/zp/leetcode/package2023/february/february20/BestHand.java
@@ -0,0 +1,46 @@
+package com.zp.leetcode.package2023.february.february20;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author zhengpeng
+ * @date 2023/2/20 9:20 上午
+ * 最好的扑克手
+ **/
+public class BestHand {
+
+    public static void main(String[] args) {
+
+        int[] rank = new int[]{13,2,3,1,9};
+        char[] suits = new char[]{'a','a','a','a','a'};
+        BestHand bestHand = new BestHand();
+        System.out.println(bestHand.bestHand(rank,suits));
+
+    }
+
+    public String bestHand(int[] ranks, char[] suits) {
+        Set<Character> suitsSet = new HashSet<Character>();
+        for (char suit : suits) {
+            suitsSet.add(suit);
+        }
+        if (suitsSet.size() == 1) {
+            return "Flush";
+        }
+        Map<Integer, Integer> h = new HashMap<Integer, Integer>();
+        for (int rank : ranks) {
+            h.put(rank, h.getOrDefault(rank, 0) + 1);
+        }
+        if (h.size() == 5) {
+            return "High Card";
+        }
+        for (Map.Entry<Integer, Integer> entry : h.entrySet()) {
+            if (entry.getValue() > 2) {
+                return "Three of a Kind";
+            }
+        }
+        return "Pair";
+    }
+}
