9ba4097edfb198c3d29d0885c9ad6795a0713b05
zhengpeng <847850277@qq.com>
2023-02-22 09:18:26 +0800

    feature: 石头游戏

---
+++ src/main/java/com/zp/leetcode/package2023/february/february22/StoneGame.java
@@ -0,0 +1,18 @@
+package com.zp.leetcode.package2023.february.february22;
+
+/**
+ * @author zhengpeng
+ * @date 2023/2/22 9:00 上午
+ **/
+public class StoneGame {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean stoneGame(int[] piles) {
+        //int sumAlice,sumBob = 0;
+        return true;
+    }
+}
