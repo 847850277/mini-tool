68be1509f3d4d95c301efd10e029fc8d1ef14f05
zhengpeng <847850277@qq.com>
2023-02-24 11:44:01 +0800

    feature: 使数组中所有元素都等于0

---
+++ src/main/java/com/zp/leetcode/package2023/february/february24/MinimumOperations.java
@@ -0,0 +1,27 @@
+package com.zp.leetcode.package2023.february.february24;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author zhengpeng
+ * @date 2023/2/24 11:37 上午
+ * 使数组中所有元素都等于零
+ **/
+public class MinimumOperations {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int minimumOperations(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums) {
+            if(num > 0){
+                set.add(num);
+            }
+        }
+        return set.size();
+    }
+}
