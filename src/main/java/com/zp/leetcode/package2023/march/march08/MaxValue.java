27f67661ac8124bcbad6bbaaff30d5b51dc74563
zhengpeng <847850277@qq.com>
2023-03-09 17:47:22 +0800

    feature: 得到K个黑块的最少涂色次数

---
+++ src/main/java/com/zp/leetcode/package2023/march/march08/MaxValue.java
@@ -0,0 +1,21 @@
+package com.zp.leetcode.package2023.march.march08;
+
+/**
+ * @author zhengpeng
+ * @date 2023/3/8 2:23 下午
+ **/
+public class MaxValue {
+
+    /**
+     * 礼物的最大价值
+     * @param grid
+     * @return
+     */
+    public int maxValue(int[][] grid) {
+
+
+
+        return 0;
+    }
+
+}
