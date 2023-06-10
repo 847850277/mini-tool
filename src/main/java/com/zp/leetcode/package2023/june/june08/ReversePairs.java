087089a95a84fd0d47c9bddf34ac78fb97efc4ad
zhengpeng <847850277@qq.com>
2023-06-10 13:30:47 +0800

    feature: 比较字符串最小字母出现的频率

---
+++ src/main/java/com/zp/leetcode/package2023/june/june08/ReversePairs.java
@@ -0,0 +1,21 @@
+package com.zp.leetcode.package2023.june.june08;
+
+/**
+ * @author zhengpeng
+ * @date 2023/6/8 16:30
+ *
+ **/
+public class ReversePairs {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public int reversePairs(int[] nums) {
+
+        return 0;
+    }
+}
---
+++ src/main/java/com/zp/leetcode/package2023/june/june10/NumSmallerByFrequency.java
@@ -0,0 +1,41 @@
+package com.zp.leetcode.package2023.june.june10;
+
+/**
+ * @author zhengpeng
+ * @date 2023/6/10 10:11
+ **/
+public class NumSmallerByFrequency {
+
+    public int[] numSmallerByFrequency(String[] queries, String[] words) {
+        int[] count = new int[12];
+        for (String s : words) {
+            count[f(s)]++;
+        }
+        for (int i = 9; i >= 1; i--) {
+            count[i] += count[i + 1];
+        }
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String s = queries[i];
+            res[i] = count[f(s) + 1];
+        }
+        return res;
+    }
+
+    public int f(String s) {
+        int cnt = 0;
+        char ch = 'z';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c < ch) {
+                ch = c;
+                cnt = 1;
+            } else if (c == ch) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+
+}
