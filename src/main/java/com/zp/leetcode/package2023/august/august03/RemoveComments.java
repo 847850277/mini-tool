e73a5bff1a7d6663f638c057d13d9e559c8497a9
zhengpeng <847850277@qq.com>
2023-08-03 14:06:45 +0800

    feature: 删除注释

---
+++ src/main/java/com/zp/leetcode/package2023/august/august03/RemoveComments.java
@@ -0,0 +1,41 @@
+package com.zp.leetcode.package2023.august.august03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 删除注释
+ */
+public class RemoveComments {
+
+    public List<String> removeComments(String[] source) {
+        List<String> res = new ArrayList<String>();
+        StringBuilder newLine = new StringBuilder();
+        boolean inBlock = false;
+        for (String line : source) {
+            for (int i = 0; i < line.length(); i++) {
+                if (inBlock) {
+                    if (i + 1 < line.length() && line.charAt(i) == '*' && line.charAt(i + 1) == '/') {
+                        inBlock = false;
+                        i++;
+                    }
+                } else {
+                    if (i + 1 < line.length() && line.charAt(i) == '/' && line.charAt(i + 1) == '*') {
+                        inBlock = true;
+                        i++;
+                    } else if (i + 1 < line.length() && line.charAt(i) == '/' && line.charAt(i + 1) == '/') {
+                        break;
+                    } else {
+                        newLine.append(line.charAt(i));
+                    }
+                }
+            }
+            if (!inBlock && newLine.length() > 0) {
+                res.add(newLine.toString());
+                newLine.setLength(0);
+            }
+        }
+        return res;
+    }
+
+}
