6f9a0142254fc42a0c652a03906c17fc3bb47de5
zhengpeng <847850277@qq.com>
2023-08-08 10:50:59 +0800

    feature: 任意子数组和的绝对值的最大值

---
+++ src/main/java/com/zp/leetcode/package2023/august/august08/MaxAbsoluteSum.java
@@ -0,0 +1,22 @@
+package com.zp.leetcode.package2023.august.august08;
+
+/**
+ * 任意子数组和的绝对值的最大值
+ */
+public class MaxAbsoluteSum {
+
+    public int maxAbsoluteSum(int[] nums) {
+        int positiveMax = 0, negativeMin = 0;
+        int positiveSum = 0, negativeSum = 0;
+        for (int num : nums) {
+            positiveSum += num;
+            positiveMax = Math.max(positiveMax, positiveSum);
+            positiveSum = Math.max(0, positiveSum);
+            negativeSum += num;
+            negativeMin = Math.min(negativeMin, negativeSum);
+            negativeSum = Math.min(0, negativeSum);
+        }
+        return Math.max(positiveMax, -negativeMin);
+    }
+
+}
