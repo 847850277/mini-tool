bda50aa48c338e2d200d4a5eea9258fc6b36d208
zhengpeng <847850277@qq.com>
2023-08-10 17:26:50 +0800

    feature: 下降路径最小和.

---
+++ src/main/java/com/zp/leetcode/package2023/august/august10/MinFallingPathSum.java
@@ -0,0 +1,42 @@
+package com.zp.leetcode.package2023.august.august10;
+
+/**
+ * 下降路径最小和II
+ */
+public class MinFallingPathSum {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length;
+        int[][] d = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                d[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            d[0][i] = grid[0][i];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (j == k) {
+                        continue;
+                    }
+                    d[i][j] = Math.min(d[i][j], d[i - 1][k] + grid[i][j]);
+                }
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            res = Math.min(res, d[n - 1][j]);
+        }
+        return res;
+
+    }
+}
