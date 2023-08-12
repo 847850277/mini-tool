451480de701ca1201b909d3e4adc2591c4257006
zhengpeng <847850277@qq.com>
2023-08-12 21:46:42 +0800

    feature: 合并K个升序链表.

---
+++ src/main/java/com/zp/leetcode/package2023/august/august12/MergeKLists.java
@@ -0,0 +1,50 @@
+package com.zp.leetcode.package2023.august.august12;
+
+public class MergeKLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; i++) {
+            ans = mergeTwoList(ans,lists[i]);
+        }
+        return ans;
+    }
+
+    private ListNode mergeTwoList(ListNode a, ListNode b) {
+        if(a == null || b == null){
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
