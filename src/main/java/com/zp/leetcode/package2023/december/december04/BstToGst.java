1bea8b6180dec4e7e0dd1afe5a23657f6c0f8d98
zhengpeng <847850277@qq.com>
2023-12-04 09:51:47 +0800

    feature: 从二叉搜索树到更大和树.

---
+++ src/main/java/com/zp/leetcode/package2023/december/december04/BstToGst.java
@@ -0,0 +1,40 @@
+package com.zp.leetcode.package2023.december.december04;
+
+public class BstToGst {
+
+
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root != null){
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+}
+
+
+
